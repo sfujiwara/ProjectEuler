56287c3046d403e1673a948fe4dbc82257e90444
Shuhei <shuhei.fujiwara@gmail.com>
2014-07-10 02:01:49 +0900

    とりあえず

---
+++ Python/prob081.py
@@ -0,0 +1,19 @@
+import csv
+
+filename = "matrix.txt"
+csvfile = open(filename)
+mat = []
+
+for row in csv.reader(csvfile):
+    mat.append([int(i) for i in row ])
+
+csvfile.close()
+
+route = [[0] * 80 for i in range(80)]
+
+for i in xrange(80):
+    for j in xrange(80):
+        if i==0 and j==0: pass
+        elif i==0: mat[i][j] = mat[i][j-1] + mat[i][j]
+        elif j==0: mat[i][j] = mat[i-1][j] + mat[i][j]
+        else: mat[i][j] = min(mat[i-1][j], mat[i][j-1]) + mat[i][j]
