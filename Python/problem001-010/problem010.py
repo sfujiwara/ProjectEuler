c435680e982db4204c90642b219eaedb775b9da3
shuhei <shuhei.fujiwara@gmail.com>
2015-02-06 03:47:26 +0900

    Made directory problem001-010

--- Python/problem001-010/problem010.py
+++ Python/problem001-010/problem010.py
@@ -3,11 +3,11 @@
 
 import math
 
-# エラトステネスの篩でn以下の素数列を返す関数
+## エラトステネスの篩でn以下の素数列を返す関数
 def sieve(n):
-    # 0-nが素数か否かのフラグ
+    ## 0-nが素数か否かのフラグ
     flag = [True]*(n+1)
-    # とりあえず0, 1は素数ではない
+    ## 0, 1 は素数ではない
     flag[0], flag[1] = False, False
     for i in xrange(2, int(math.sqrt(n))+1):
         if flag[i]:
