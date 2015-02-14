163bfabb7eb0117c8a68d767f50e6d621a314607
shuhei <shuhei.fujiwara@gmail.com>
2015-02-14 21:22:59 +0900

    Modified problem 11 and 20

--- Python/problem011-020/problem012.py
+++ Python/problem011-020/problem012.py
@@ -1,5 +1,11 @@
 # -*- coding: utf-8 -*-
 
+'''
+三角数は n(n+1)/2 と書くことができる.
+n が偶数の場合, n と (n+1)/2 は互いに素なので, 分割して素因数分解すれば良い.
+n が奇数の場合は n/2 と (n+1) に分ける.
+'''
+
 ## Prime Factor Decomposition
 ## 出力は Ruby の prime_division と同じ
 ## x^a * y^b * z^c -> [[x, a], [y, b], [z, c]]
@@ -36,11 +42,19 @@ def pfd(n):
 if __name__ == '__main__':
     i = 2
     while True:
-        prime_factors = pfd(i*(i+1)/2)
-        num_of_factors = 1
-        for j in xrange(len(prime_factors)):
-            num_of_factors *= (prime_factors[j][1]+1)
-        if num_of_factors > 500:
-            print "Answer:", i*(i+1)/2
+        if i % 2 == 0:
+            prime_factors1 = pfd(i / 2)
+            prime_factors2 = pfd(i + 1)
+        else:
+            prime_factors1 = pfd(i)
+            prime_factors2 = pfd((i + 1) / 2)
+        num_of_factors1 = 1
+        for j in xrange(len(prime_factors1)):
+            num_of_factors1 *= (prime_factors1[j][1] + 1)
+        num_of_factors2 = 1
+        for j in xrange(len(prime_factors2)):
+            num_of_factors2 *= (prime_factors2[j][1] + 1)
+        if num_of_factors1 * num_of_factors2 > 500:
+            print "Answer:", i * (i + 1) / 2
             break
         i += 1
